1d697eb961440419f15e8b41d1042237b1dbf44d
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-01 23:09:54 -0800

    add uv_matrix.h

---
+++ include/uv_matrix.h
@@ -0,0 +1,24 @@
+#ifndef PILOTWINGS64_UV_MATRIX
+#define PILOTWINGS64_UV_MATRIX
+
+#include <ultra64.h>
+
+typedef float Mtx4F_t[4][4];
+
+typedef union {
+    Mtx4F_t mf;
+    struct {
+        float xx, yx, zx, wx,
+              xy, yy, zy, wy,
+              xz, yz, zz, wz,
+              xw, yw, zw, ww;
+    };
+} MtxF;
+
+void Mat4_Copy(Mtx4F_t m_dst, Mtx4F_t m_src);
+void Mat3_Copy(Mtx4F_t m_dst, Mtx4F_t m_src);
+void Mat4_SetIdentity(Mtx4F_t arg0);
+void Mat4_SetUnk1(Mtx_t mat);
+
+#endif // PILOTWINGS64_UV_MATRIX
+
