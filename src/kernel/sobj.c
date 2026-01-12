ca32aac8ba381e6c8ee7e147bfff724217bca702
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-11 16:14:59 -0800

    rename sobj.c, texture.c

---
+++ src/kernel/sobj.c
@@ -0,0 +1,37 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/uvSobjsDraw.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022C7B8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022C8D0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022CC28.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/uvSobjLoadTerra.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/uvSobjPosm.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022D168.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022D1E4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/uvSobjModel.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/strchr.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/strlen.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022D338.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022D4D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022D810.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022DA40.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022DA6C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022DA9C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/sobj/func_8022DE64.s")
