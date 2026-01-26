1143906da64dc30d9144a66aaa81d3a11599f72a
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-26 01:41:19 -0800

    rename snap.c, snow.c

---
+++ src/app/snow.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/snow/snow_create.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/snow/snow_draw.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/snow/snow_update.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/snow/func_803404D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/snow/func_803405C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/snow/func_803405E4.s")
