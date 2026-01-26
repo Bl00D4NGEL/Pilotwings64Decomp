abc34e1f5fed7ddad0d20e767884a5bdb4c9ae75
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-25 23:31:37 -0800

    rodata fixes

--- src/app/code_60020.c
+++ src/app/code_60020.c
@@ -3,3 +3,19 @@
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D8AF0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D9220.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D9430.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D94EC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D95D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D9818.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D9900.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D9AB8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D9B40.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D9BBC.s")
