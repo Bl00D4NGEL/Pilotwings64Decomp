1261d318678156ec31f04923e0729eaaad195cd9
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-26 21:41:55 -0800

    discord integration

--- tools/discord_progress.py
+++ tools/discord_progress.py
@@ -3,14 +3,11 @@ import requests
 import subprocess
 from git import Repo
 
-import decomp_settings
-import mapfile_parser
-
 repo = Repo('.')
 
 def main(args):
     progress = subprocess.run(
-        [ "python3", "-m", "mapfile_parser", "objdiff_report", "--version", "us" ],
+        [ "python3", "-m", "mapfile_parser", "objdiff_report", "--version", args.version ],
         capture_output=True, text=True, check=True
     )
     progress_msg = progress.stdout
@@ -26,11 +23,16 @@ def main(args):
             "color": commit.authored_date & 0xFFFFFF
         }]
     }
-    requests.post(args.discord_url, json=content)
+    if args.skip_post:
+        print(f"content: {content}");
+    else:
+        requests.post(args.discord_url, json=content)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Publish objdiff_report to Discord server")
     parser.add_argument("--discord_url", help="Discord webhook URL", required=True)
+    parser.add_argument("--version", help="Select ROM version", default="us")
+    parser.add_argument("--skip_post", help="Skip the post step", action='store_true')
     args = parser.parse_args()
     main(args)
 
