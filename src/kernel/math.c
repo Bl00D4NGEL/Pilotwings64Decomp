c282c0e976a39156f762488e35f899b706fcb171
Garrett Smith <smith.garrett.c@gmail.com>
2026-02-02 12:38:01 -0800

    rename code_2AE50 => math

---
+++ src/kernel/math.c
@@ -0,0 +1,14 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/math/SqrtF.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/math/func_80229EC0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/math/func_8022A080.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/math/Length2D.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/math/Length3D.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/kernel/math/func_8022A27C.s")
+
