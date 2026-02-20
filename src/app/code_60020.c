846d2d32a2888a579f738502753371a75d746b4a
Bl00D4NGEL <github@d-peters.com>
2026-02-20 21:48:42 +0100

    map .data for app/code_60020

--- src/app/code_60020.c
+++ src/app/code_60020.c
@@ -2,6 +2,16 @@
 
 #include "code_60020.h"
 
+f32 D_8034EA10 = 0.0f;
+f32 D_8034EA14 = 0.0f;
+f32 D_8034EA18 = 0.0f;
+f32 D_8034EA1C = 0.0f;
+f32 D_8034EA20 = 0.0f;
+f32 D_8034EA24 = 0.0f;
+s32 padD_8034EA28 = 0;
+s32 padD_8034EA2C = 0;
+Vec3F D_8034EA30 = { 0.0f, 0.0f, 0.0f }; // used for assignment in func_802D9430
+
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D8AF0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_60020/func_802D9220.s")
