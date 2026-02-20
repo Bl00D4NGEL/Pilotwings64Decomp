e58d1aa0d7668d80d1665dd9176399d7a0a84ae2
Bl00D4NGEL <github@d-peters.com>
2026-02-20 22:14:04 +0100

    map .data for app/code_94E60

--- src/app/code_94E60.c
+++ src/app/code_94E60.c
@@ -1,5 +1,19 @@
 #include "common.h"
 
+
+s8 D_8034F7A0 = 0;
+s8 D_8034F7A4 = 0;
+u8 D_8034F7A8 = 0xFF;
+u8 D_8034F7AC[4] = { 0xFF, 0, 0, 0};
+u8 D_8034F7B0 = 0;
+s8 D_8034F7B4[4] = { 0 }; // might be u8
+u8 D_8034F7B8 = 0;
+u8 D_8034F7BC = 0;
+u8 D_8034F7C0[4] = { 1, 0, 0, 0 };
+f32 D_8034F7C4[6] = { -68.08f, -302.14f, 10.74f, -0.0174533f, 0.1f, 0.0f };
+f32 D_8034F7DC = 0.0f;
+f32 D_8034F7E0 = 1.6f;
+
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_94E60/func_8030D930.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_94E60/func_8030D9C8.s")
