495238e13513769507e9417dac715aed292433e4
Bl00D4NGEL <github@d-peters.com>
2026-02-21 01:15:10 +0100

    map .data for app/code_64730 and add comments for other data locations

--- src/app/code_64730.c
+++ src/app/code_64730.c
@@ -1,4 +1,23 @@
 #include "common.h"
+#include "code_5A6A0.h"
+
+s8 D_8034EDA0 = 0;
+s8 D_8034EDA4[] = { 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+f32 D_8034EDBC = 0.0f;
+
+u8 D_8034EDC0 = 0xFF;
+u8 D_8034EDC4 = 0xFF;
+s32 D_8034EDC8 = 0xFFFF;
+s32 D_8034EDCC = 0xFFFF;
+s32 D_8034EDD0 = 0;
+s32 D_8034EDD4 = 0;
+s32 D_8034EDD8 = 0;
+
+Unk803599D0 D_8034EDDC[] = { 8, 0.36f, 0.0f, 0.412f, 0.1f, 0.465f, 0.25f, 0.517f, 0.475f, 0.568f, 0.5f, 0.62f, 0.475f, 0.673f, 0.25f, 0.725f, 0.0f };
+
+s32 padD_8034ED30[4] = { 0 };
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_64730/func_802DD200.s")
 
