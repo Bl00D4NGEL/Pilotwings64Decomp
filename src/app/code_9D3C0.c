a0f95ed7d3bce026f98bbef25151a83975fabc02
Bl00D4NGEL <github@d-peters.com>
2026-02-23 01:26:59 +0100

    map .data for app/code_9D3C0, app/pads, app/hud, app/demo_attitude

--- src/app/code_9D3C0.c
+++ src/app/code_9D3C0.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+f32 D_8034F8D0[] = { 0.0f, 0.2f, 0.4f, 0.6f, 1.0f };
+s32 D_8034F8E4 = 0;
+s32 D_8034F8E8 = 0;
+s32 D_8034F8EC = 4;
+s32 D_8034F8F0 = 4;
+s32 D_8034F8F4 = 0;
+
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_9D3C0/func_80315E90.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/code_9D3C0/func_80315F0C.s")
--- src/app/pads.c
+++ src/app/pads.c
@@ -1,6 +1,9 @@
 #include "common.h"
 #include "pads.h"
 
+s8 D_8034F900 = 0;
+u16 D_8034F904[] = { 0x0102, 0x0103, 0x0104, 0x0000, 0x0000, 0x0000 };
+
 #pragma GLOBAL_ASM("asm/nonmatchings/app/pads/func_80316DC0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/pads/func_80316E40.s")
